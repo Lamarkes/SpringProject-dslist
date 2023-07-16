c077fb761347d97b2b06ff1c23d0740736b8e1be
Lamark <lamark12ricarte@gmail.com>
2023-07-16 14:41:18 -0300

    adicionando o GET game by id

--- src/main/java/com/reviwgames/dslist/controllers/GameController.java
+++ src/main/java/com/reviwgames/dslist/controllers/GameController.java
@@ -1,10 +1,11 @@
 package com.reviwgames.dslist.controllers;
 
+import com.reviwgames.dslist.dto.GameDTO;
 import com.reviwgames.dslist.dto.GameMinDTO;
-import com.reviwgames.dslist.entities.Game;
 import com.reviwgames.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,14 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    //GetMapping-> usado para mapear requisicoes GET usadas nos controllers
+    //passando um id como argumento na rota, que sera redirecionado para as configuracoes da classe
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
--- src/main/java/com/reviwgames/dslist/services/GameService.java
+++ src/main/java/com/reviwgames/dslist/services/GameService.java
@@ -1,11 +1,13 @@
 package com.reviwgames.dslist.services;
 
 
+import com.reviwgames.dslist.dto.GameDTO;
 import com.reviwgames.dslist.dto.GameMinDTO;
 import com.reviwgames.dslist.entities.Game;
 import com.reviwgames.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +17,12 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get(); // busca o game por id e armazena na variavel result
+        return new GameDTO(result); // converte o game na variavel result em um gameDTO
+    }
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(x -> new GameMinDTO(x)).toList();
