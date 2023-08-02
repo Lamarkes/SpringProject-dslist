623250e635b3899d0a1f3455550e16eb3e4547f2
Lamark <lamark12ricarte@gmail.com>
2023-08-01 23:57:31 -0300

    move games on list

--- src/main/java/com/reviwgames/dslist/controllers/GameListController.java
+++ src/main/java/com/reviwgames/dslist/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.reviwgames.dslist.controllers;
 
 import com.reviwgames.dslist.dto.GameListDTO;
 import com.reviwgames.dslist.dto.GameMinDTO;
+import com.reviwgames.dslist.dto.ReplacementDTO;
 import com.reviwgames.dslist.services.GameListService;
 import com.reviwgames.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,4 +35,8 @@ public class GameListController {
         return result;
     }
 
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+        gameListService.move(listId,body.getSourceIndex(),body.getDestinationIndex());
+    }
 }
